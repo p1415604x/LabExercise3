3b9a1be20dfda7dd54c9b4b881c29d6295c7bb97
Laurynas Mykolaitis <P1415604X@KLIXX201LIN007W.lec-admin.dmu.ac.uk>
2016-02-02 14:53:51 +0000

    Testing branch: added MIN/MAX functionality

--- maths_function/maths_function.c
+++ maths_function/maths_function.c
@@ -4,13 +4,24 @@
 
 int main()
 {
-    int c;
+    int c,i,a[5],min,max;
+	min=999;
+	max=0;
     printf("2+2=%i\n", c=2+2);
     printf("2-2=%i\n", c=2-2);
     printf("2*2=%i\n", c=2*2);
     printf("2/2=%i\n", c=2/2);
     printf("3%2=%i\n", c=3%2);
-    printf("Enough of those math functions. <EXIT>");
+    printf("Enter 5 variables");
+    for(i=0; i<5; i++) {
+	scanf("%i"),&a[i]);	}
 
+    for(i=5; i>0; i--) {
+	if (max < a[i]) 
+	max = a[i];}	
+
+ 	for(i=5; i>0; i--) {
+		if (min > a[i]) 
+		min = a[i];}		
     return 0;
 }
